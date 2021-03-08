6d38bca7851466c38f190a989f20a29e61c107f9
Jution Candra Kirana <jution.kirana@enigmacamp.com>
2021-03-08 16:42:56 +0700

    Menambahkan lifecycle pada setiap activity

--- app/src/main/java/com/enigmacamp/gold_pocket/BuyActivity.kt
+++ app/src/main/java/com/enigmacamp/gold_pocket/BuyActivity.kt
@@ -3,10 +3,15 @@ package com.enigmacamp.gold_pocket
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 
 class BuyActivity : AppCompatActivity() {
+
+    private val ACTIVITY_NAME = "BuyActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(ACTIVITY_NAME, "OnCreate()")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buy)
     }
@@ -14,5 +19,30 @@ class BuyActivity : AppCompatActivity() {
     fun handleBack(view: View) {
         finish()
         //onBackPressed()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(ACTIVITY_NAME, "OnStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(ACTIVITY_NAME, "OnResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(ACTIVITY_NAME, "OnPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(ACTIVITY_NAME, "OnStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(ACTIVITY_NAME, "OnDestroy()")
     }
 }
--- app/src/main/java/com/enigmacamp/gold_pocket/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/gold_pocket/MainActivity.kt
@@ -3,12 +3,16 @@ package com.enigmacamp.gold_pocket
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    private val ACTIVITY_NAME = "Activity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(ACTIVITY_NAME, "OnCreate()")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buyProduct.setOnClickListener(this)
@@ -26,4 +30,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             historyProduct -> {}
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(ACTIVITY_NAME, "OnStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(ACTIVITY_NAME, "OnResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(ACTIVITY_NAME, "OnPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(ACTIVITY_NAME, "OnStop()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(ACTIVITY_NAME, "OnDestroy()")
+    }
+
+
 }
