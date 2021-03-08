87865eddee8e95d381f894b9dc38d53e6d0a3606
Jution Candra Kirana <jution.kirana@enigmacamp.com>
2021-03-08 16:36:44 +0700

    Mencoba membuat 3 buah fragment dalam 1 activity

--- app/src/main/java/com/enigmacamp/gold_pocket/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/gold_pocket/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.enigmacamp.gold_pocket
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var buyFragment: BuyFragment
+    lateinit var sellFragment: SellFragment
+    lateinit var historyFragment: HistoryFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,16 +18,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         buyProduct.setOnClickListener(this)
         sellProduct.setOnClickListener(this)
         historyProduct.setOnClickListener(this)
+        buyFragment = BuyFragment()
+        sellFragment = SellFragment()
+        historyFragment = HistoryFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, buyFragment)
+            .commit()
     }
 
     override fun onClick(v: View?) {
         when(v) {
             buyProduct -> {
-                val intent = Intent(this, BuyActivity::class.java)
-                startActivity(intent)
+                switchFragment(buyFragment)
             }
-            sellProduct -> {}
-            historyProduct -> {}
+            sellProduct -> {
+                switchFragment(sellFragment)
+            }
+            historyProduct -> {
+                switchFragment(historyFragment)
+            }
         }
+    }
+
+    private fun switchFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, fragment).commit()
     }
 }
